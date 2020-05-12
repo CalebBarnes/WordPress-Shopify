d37653a7f9a799411264be9ffa0facd4639cf5ab
CalebBarnes <caleb.d.barnes@gmail.com>
2020-05-12 02:06:28 -0700

    Create get-attachment-by-post-name.php

---
+++ includes/functions/get-attachment-by-post-name.php
@@ -0,0 +1,21 @@
+<?php
+
+if( ! ( function_exists( 'wp_get_attachment_by_post_name' ) ) ) {
+    function wp_get_attachment_by_post_name( $post_name ) {
+            $args           = array(
+                'posts_per_page' => 1,
+                'post_type'      => 'attachment',
+                'name'           => trim( $post_name ),
+            );
+
+            $get_attachment = new WP_Query( $args );
+            
+            if ( ! $get_attachment || ! isset( $get_attachment->posts, $get_attachment->posts[0] ) ) {
+                return false;
+            }
+
+            return $get_attachment->posts[0];
+    }
+}
+
+?>
